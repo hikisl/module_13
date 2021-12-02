8adb19be0195794657838e178f704d2c3cca7121
oldboyee <fuukingy@gmail.com>
2021-12-02 17:40:18 +0200

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace task1
 {
@@ -6,7 +9,54 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\oldboyee\Desktop\Text1.txt";
+
+            var list = new List<string>();
+            var linkedList = new LinkedList<string>();
+
+            var stopWatch1 = Stopwatch.StartNew();
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    var signs = sr.ReadToEnd();
+                    foreach (var item in signs.Split(' ', '!', '\''))
+                    {
+                        list.Add(item);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine($"Производительность вставки в List<T>: {stopWatch1.Elapsed.TotalMilliseconds} мс. ");
+
+            Console.WriteLine();
+
+            var stopWatch2 = Stopwatch.StartNew();
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    var signs = sr.ReadToEnd();
+
+                    foreach (var item in signs.Split(' ', '!', '\''))
+                    {
+                    linkedList.AddFirst(item);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           Console.WriteLine($"Производительность вставки в LinkedList<T>: {stopWatch2.Elapsed.TotalMilliseconds} мс.");
+
+            Console.ReadLine();
         }
+
     }
 }
